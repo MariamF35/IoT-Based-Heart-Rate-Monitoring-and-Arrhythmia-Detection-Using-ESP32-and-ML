839bc581794ad82a98b02610346bb81ded1504fb
Mariam F <133355222+MariamF35@users.noreply.github.com>
2026-02-05 00:08:31 +0530

    Create main.py

---
+++ apps/main.py
@@ -0,0 +1,28 @@
+#Python code
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("bidmc_data.csv")
+features = ['HR', 'RR_interval', 'SDNN']
+df = df.dropna()
+
+X = df[features]
+y = df['label']   # 0 = Normal, 1 = Abnormal
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
